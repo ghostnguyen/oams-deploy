89ce44318b369a9a42c96c4fbcad43427802af20
ghostnguyen <ghostnguyen@gmail.com>
2011-10-10 15:41:24 +0000

    deploy

--- Cmd/dev_commit_deploy.bat
+++ Cmd/dev_commit_deploy.bat
@@ -1,5 +1,6 @@
-del D:\ghostnguyen\oams-deploy\app_data /f /q
-rd D:\ghostnguyen\oams-deploy\app_data
-7za a -r -x!*.svn D:\ghostnguyen\oams-deploy\oams-deploy.zip D:\ghostnguyen\oams-deploy\*.*
+del D:\ghostnguyen\oams-deploy\bin\app_data /f /q
+rd D:\ghostnguyen\oams-deploy\bin\app_data
+7za a -r -x!*.svn D:\ghostnguyen\oams-deploy\oams-deploy.zip D:\ghostnguyen\oams-deploy\bin\*.*
 svn.exe commit D:\ghostnguyen\oams-deploy\ -m "deploy"
 svn.exe update D:\ghostnguyen\oams-deploy\
+pause
